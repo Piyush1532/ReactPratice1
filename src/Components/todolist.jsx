0ef655be91d08d73c3ef1dc707a390d805fb46ab
Piyush1532 <piyushadav503@gmail.com>
2024-02-13 13:41:17 +0530

    h

--- src/Components/todolist.jsx
+++ src/Components/todolist.jsx
@@ -5,7 +5,7 @@ const TodoList = (props) => {
   return (
     <div>
         <h1 className='text-center'>todolist</h1>
-        
+        <h2>hello world Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, doloribus ut molestiae, iure nemo accusamus, animi doloremque fugiat nobis possimus at. Ut quas quo impedit ad voluptatum quaerat modi mollitia.</h2>
         {todoList.map((value,index)=>{
 return <h1 key={index} className=' list'>{value.name}</h1>
       })}
